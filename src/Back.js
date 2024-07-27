2d159cd86df10a26e25ec5372a7d570592aa9e06
stevepurpose <stevenpurpose@gmail.com>
2024-07-27 06:01:51 +0100

    typi error corrected in env variable

--- src/Back.js
+++ src/Back.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import axios from "axios";
 
-let my_api = process.env.REACT_APP_BACKEND_API_UR
+let my_api = process.env.REACT_APP_BACKEND_API_URL
    const BACKENDURL=axios.create({
     baseURL:my_api
   
